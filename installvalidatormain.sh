4cc04bcd2be7a7fd28f0d7902581f6a80e3c0b91
Liudmyla | Mrs_ml <118111977+milakorn69@users.noreply.github.com>
2024-12-23 11:31:29 +0200

    Update installvalidatormain.sh
    v3.2.0

--- installvalidatormain.sh
+++ installvalidatormain.sh
@@ -27,7 +27,7 @@ cd $HOME
 rm -rf celestia-app 
 git clone https://github.com/celestiaorg/celestia-app.git 
 cd celestia-app/ 
-APP_VERSION=v3.1.1 
+APP_VERSION=v3.2.0 
 git checkout tags/$APP_VERSION -b $APP_VERSION 
 make install
 make configure-v3
