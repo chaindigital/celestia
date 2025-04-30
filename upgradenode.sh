acbf100e4809c92ad2a1cf19b74408ae36681379
Liudmyla | Mrs_ml <118111977+milakorn69@users.noreply.github.com>
2025-04-30 18:21:47 +0300

    Update upgradenode.sh

--- upgradenode.sh
+++ upgradenode.sh
@@ -3,7 +3,7 @@ sudo systemctl stop celestia-bridge
 rm -rf celestia-node
 git clone https://github.com/celestiaorg/celestia-node.git
 cd celestia-node/
-git checkout tags/v0.22.1-mocha
+git checkout tags/v0.22.2-mocha
 make build 
 sudo make install 
 make cel-key 
