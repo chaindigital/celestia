98b7381d00da683c5fda230313ae4e17d5ecaf31
Liudmyla | Mrs_ml <118111977+milakorn69@users.noreply.github.com>
2025-06-04 22:51:24 +0300

    Update installfulltest.sh

--- installfulltest.sh
+++ installfulltest.sh
@@ -6,7 +6,7 @@ cd ~
 rm -rf $HOME/go
 sudo rm -rf /usr/local/go
 cd $HOME
-curl https://dl.google.com/go/go1.22.5.linux-amd64.tar.gz | sudo tar -C/usr/local -zxvf -
+curl https://dl.google.com/go/go1.24.1.linux-amd64.tar.gz | sudo tar -C/usr/local -zxvf -
 cat <<'EOF' >>$HOME/.profile
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
