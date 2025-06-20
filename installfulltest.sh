caec5cf40f71d039d14d012f358a29aabaa2d0f3
Liudmyla | Mrs_ml <118111977+milakorn69@users.noreply.github.com>
2025-06-21 00:50:05 +0300

    Update installfulltest.sh

--- installfulltest.sh
+++ installfulltest.sh
@@ -17,7 +17,7 @@ source $HOME/.profile
 go version
 
 git clone https://github.com/celestiaorg/celestia-app && cd celestia-app
-git checkout v4.0.2-mocha
+git checkout v4.0.3-mocha
 make install
 
 cd $HOME
