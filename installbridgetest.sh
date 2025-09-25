62185602daa71af9eaf43db184b64939dfddb62a
Liudmyla | Mrs_ml <118111977+milakorn69@users.noreply.github.com>
2025-09-25 20:04:56 +0300

    Update installbridgetest.sh

--- installbridgetest.sh
+++ installbridgetest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone https://github.com/celestiaorg/celestia-node && cd celestia-node
 
-git checkout tags/v0.25.3-mocha
+git checkout tags/v0.27.2-mocha
 
 make build
 make install
