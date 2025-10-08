f0645de33ca7a8ffdf94c0c464ffbde0924506c2
CHAIN DIGITAL <102434694+chaindigital@users.noreply.github.com>
2025-10-09 00:29:40 +0300

    Update upgradenodemain.sh

--- upgradenodemain.sh
+++ upgradenodemain.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo systemctl stop celestia-bridge
+cd $HOME
 rm -rf celestia-node
 git clone https://github.com/celestiaorg/celestia-node.git
-cd celestia-node/
-git checkout tags/v0.27.4-mocha
+cd celestia-node
+git checkout tags/v0.26.4 
 make build 
 sudo make install 
-make cel-key 
-celestia bridge config-update --p2p.network mocha
-systemctl daemon-reload
-systemctl restart celestia-bridge && journalctl -u celestia-bridge -f -o cat
+make cel-key
+celestia bridge config-update
+sudo systemctl restart celestia-bridge && sudo journalctl -u celestia-bridge -f
