3c523bb5c69c7bafe27837e6b4d9db1f87ff5ba5
CHAIN DIGITAL <102434694+chaindigital@users.noreply.github.com>
2025-10-15 19:20:14 +0300

    Update installvalidatortest.sh

--- installvalidatortest.sh
+++ installvalidatortest.sh
@@ -3,7 +3,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
 
 sudo rm -rf /usr/local/go
-curl -Ls https://go.dev/dl/go1.24.1.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+curl -Ls https://go.dev/dl/go1.24.6.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
 eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
 eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
