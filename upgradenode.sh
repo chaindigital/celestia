134cdc97b507e09b8df3cb88d3de30dee7bbe518
CHAIN DIGITAL <102434694+chaindigital@users.noreply.github.com>
2025-10-28 09:22:22 +0200

    Update upgradenode.sh

--- upgradenode.sh
+++ upgradenode.sh
@@ -8,5 +8,4 @@ make build
 sudo make install 
 make cel-key 
 celestia bridge config-update --p2p.network mocha
-systemctl daemon-reload
 systemctl restart celestia-bridge && journalctl -u celestia-bridge -f -o cat
