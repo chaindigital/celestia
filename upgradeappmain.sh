648f4db823b55b948a1ba3e0bacab6951799c276
CHAIN DIGITAL <102434694+chaindigital@users.noreply.github.com>
2025-11-11 20:03:16 +0200

    Update upgradeappmain.sh

--- upgradeappmain.sh
+++ upgradeappmain.sh
@@ -3,7 +3,7 @@ cd $HOME
 rm -rf celestia-app 
 git clone https://github.com/celestiaorg/celestia-app.git 
 cd celestia-app/ 
-APP_VERSION=v5.0.11
+APP_VERSION=v6.2.4
 git checkout tags/$APP_VERSION -b $APP_VERSION 
 make install
 sudo systemctl restart celestia-appd && sudo journalctl -u celestia-appd -f
