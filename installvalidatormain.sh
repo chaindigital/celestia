7dee26ae8633309732c1aa40131fae62b289c9b0
CHAIN DIGITAL <102434694+chaindigital@users.noreply.github.com>
2025-12-09 10:53:33 +0200

    Update installvalidatormain.sh

--- installvalidatormain.sh
+++ installvalidatormain.sh
@@ -27,7 +27,7 @@ cd $HOME
 rm -rf celestia-app 
 git clone https://github.com/celestiaorg/celestia-app.git 
 cd celestia-app/ 
-APP_VERSION=v6.2.4 
+APP_VERSION=v6.3.0 
 git checkout tags/$APP_VERSION -b $APP_VERSION 
 make install
 make configure-v3
